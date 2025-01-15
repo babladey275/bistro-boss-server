aa6673635892e47c5ad98bb84849d1d525994d63
babladey275 <babladey275@gmail.com>
2025-01-15 15:49:19 +0600

    added routes to get and delete users

--- index.js
+++ index.js
@@ -36,6 +36,11 @@ async function run() {
     const cartCollection = client.db("bistroDB").collection("carts");
 
     // users related api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
 
@@ -51,6 +56,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // menu related apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
