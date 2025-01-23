d6e43f1d4638c5630ed30243657536f59f8acab5
babladey275 <babladey275@gmail.com>
2025-01-23 23:24:37 +0530

    added admin stats API endpoint with revenue calculation using aggregation

--- index.js
+++ index.js
@@ -242,6 +242,43 @@ async function run() {
 
       res.send({ paymentResult, deleteResult });
     });
+
+    // stats on analytics
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      //this is not the best way
+      // const payments = await paymentCollection.find().toArray();
+      // const revenue = payments.reduce(
+      //   (total, payment) => total + payment.price,
+      //   0
+      // );
+
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+          { $project: { totalRevenue: { $round: ["$totalRevenue", 2] } } },
+        ])
+        .toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+      res.send({
+        users,
+        menuItems,
+        orders,
+        revenue,
+      });
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
